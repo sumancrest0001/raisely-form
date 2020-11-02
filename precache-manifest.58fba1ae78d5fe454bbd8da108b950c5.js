41131d7137c9e4d61bb0d41fad7d3d7e713ab7ec
sumancrest0001 <suman.crest0001@gmail.com>
2020-11-03 00:20:39 +0545

    Updates

---
+++ precache-manifest.58fba1ae78d5fe454bbd8da108b950c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb1796fb8703eed204449de508f11c1d",
+    "url": "/https://sumancrest0001.github.io/raisely-form/index.html"
+  },
+  {
+    "revision": "63dc3afb327001946529",
+    "url": "/https://sumancrest0001.github.io/raisely-form/static/css/main.5bb361f2.chunk.css"
+  },
+  {
+    "revision": "73208bbf83c001ad5d13",
+    "url": "/https://sumancrest0001.github.io/raisely-form/static/js/2.15e30724.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/https://sumancrest0001.github.io/raisely-form/static/js/2.15e30724.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63dc3afb327001946529",
+    "url": "/https://sumancrest0001.github.io/raisely-form/static/js/main.5f77dd17.chunk.js"
+  },
+  {
+    "revision": "40bd127321a5891c4362",
+    "url": "/https://sumancrest0001.github.io/raisely-form/static/js/runtime-main.4fcc024e.js"
+  }
+]);
